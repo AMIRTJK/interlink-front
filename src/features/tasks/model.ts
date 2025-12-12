152432102d41165f1d243644080b49ecf1383668
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2025-12-12 21:36:32 +0500

    fx

--- src/features/tasks/model.ts
+++ src/features/tasks/model.ts
@@ -1,23 +1,6 @@
-import type { Dayjs } from 'dayjs';
-
-export interface TaskFormValues {
+export interface CreateTaskPayload {
   title: string;
-  date?: Dayjs;
-  time?: Dayjs;
-}
-
-export interface Participant {
-  id: string;
-  name: string;
-  avatar?: string;
-}
-
-export interface Task {
-  id: string;
-  title: string;
-  date: string;
-  time: string;
-  endTime?: string;
-  category?: 'work' | 'personal' | 'important' | 'meeting';
-  participants?: Participant[];
+  description: string;
+  status: string;
+  assignees: number[];
 }
