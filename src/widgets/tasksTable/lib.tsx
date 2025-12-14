7eec1e18314c680d7587a5c614d8c33d5318bdbf
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2025-12-14 20:48:27 +0500

    fx

--- src/widgets/tasksTable/lib.tsx
+++ src/widgets/tasksTable/lib.tsx
@@ -49,6 +49,7 @@ export const StatusIcon = ({
 	}
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const formatDate = (dateString: string | null) => {
 	if (!dateString) return "";
 	const date = new Date(dateString);
