706369f91fe6003002b355eb54e41343ae1e43bd
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2025-12-25 17:11:25 +0500

    fx

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,11 +1,9 @@
-import dayjs from "dayjs";
-import { DetailsOfLetter } from "@features/detailsOfLetter/DetailsOfLetter";
 import { Profile } from "@features/profile";
-import { LetterExecution } from "@features/ResolutionCard";
-import { ResolutionOfLetter } from "@features/ResolutionOfLetter";
 import { ModuleMenu } from "@widgets/layout";
 import { Header } from "@widgets/layout/Header";
-
+// import { LetterExecution } from "@features/ResolutionCard";
+// import { DetailsOfLetter } from "@features/detailsOfLetter/DetailsOfLetter";
+// import { ResolutionOfLetter } from "@features/ResolutionOfLetter";
 export const ProfilePage = () => {
     return (
         <div className="bg-[#e5e9f5] h-screen overflow-auto">
@@ -18,12 +16,11 @@ export const ProfilePage = () => {
             <div className="main-content-area bg-[#e5e9f5] mb-4">
                 <Profile />
             </div>
-            
-            <div className="flex flex-col gap-8 p-6 items-center">
+            {/* <div className="flex flex-col gap-8 p-6 items-center">
                 <ResolutionOfLetter />
                 <DetailsOfLetter mode="create" />
                 <LetterExecution />
-            </div>
+            </div> */}
         </div>
     );
 };
