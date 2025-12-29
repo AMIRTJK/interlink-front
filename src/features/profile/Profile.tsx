e7c05bc3b369a1a946c028b071fd0a3a1d0ee577
Developer <dev@reaction.local>
2025-12-29 10:18:20 +0500

    fx

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,5 +1,4 @@
-import { Avatar, Modal, Switch } from "antd";
-import { UserOutlined, SettingOutlined } from "@ant-design/icons";
+
 import { Outlet, useNavigate } from "react-router-dom";
 import { profileRightNav } from "./model";
 import { AppRoutes } from "@shared/config/AppRoutes";
@@ -7,9 +6,8 @@ import { tokenControl, useTheme } from "@shared/lib";
 import { ApiRoutes, _axios } from "@shared/api";
 import { IUser } from "@entities/login";
 import { useEffect, useState } from "react";
-import userAvatar from "../../assets/images/user-avatar.jpg";
-import { Loader, Tabs, SnowOverlay } from "@shared/ui";
-import { useCurrentTab } from "./lib";
+import { useCurrentTab } from "./lib/lib";
+import { RenderJSX } from "./lib/RenderJSX";
 import "./style.css";
 
 export const Profile = () => {
@@ -33,7 +31,7 @@ export const Profile = () => {
     const fetchUser = async () => {
       try {
         const response = await _axios<{ data: IUser }>(
-          `${ApiRoutes.FETCH_USER_BY_ID}${userId}`,
+          `${ApiRoutes.FETCH_USER_BY_ID}${userId} `,
           {
             method: "GET",
           }
@@ -63,82 +61,18 @@ export const Profile = () => {
     }
   };
 
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
-    <div className="flex flex-col lg:flex-row gap-6 p-4 lg:p-6">
-      <SnowOverlay enabled={isSnowEnabled} />
-      <Modal
-        title="Настройки"
-        open={isSettingsOpen}
-        onCancel={() => setIsSettingsOpen(false)}
-        footer={null}
-        width={300}
-      >
-        <div className="flex items-center justify-between py-2">
-          <span>Включить снег ❄️</span>
-          <Switch checked={isSnowEnabled} onChange={setIsSnowEnabled} />
-        </div>
-        {/* Класс для скрывания no-animations функционал:
-          "hidden-no-animations-switcher"
-        */}
-        <div className="flex items-center justify-between py-2 hidden-no-animations-switcher">
-          {/* Это пока думаю лучше скрыть   */}
-          <span>Анимации ⚡</span>
-          <Switch checked={isAnimEnabled} onChange={setIsAnimEnabled} />
-        </div>
-      </Modal>
-
-      <aside className="w-full lg:w-[28%]">
-        <div className="bg-white p-6 rounded-xl shadow">
-          <div className="flex justify-end">
-            <SettingOutlined
-              className="text-gray-500! hover:text-blue-600! transition-colors! cursor-pointer!"
-              style={{ fontSize: "20px" }}
-              onClick={() => setIsSettingsOpen(true)}
-            />
-          </div>
-          <div className="flex flex-col items-center mb-4 relative">
-            <div className="relative">
-              <Avatar
-                src={userAvatar}
-                size={128}
-                icon={<UserOutlined />}
-                className="profile-avatar"
-              />
-            </div>
-          </div>
-          <p className="text-center text-[#0037AF] text-xl font-semibold mb-6">{`${userData?.first_name} ${userData?.last_name}`}</p>
-          <div className="flex justify-between text-sm">
-            <div className="space-y-2 text-black font-light">
-              <p>Место работы:</p>
-              <p>Должность:</p>
-              <p>Номер телефона:</p>
-              <p>ИНН:</p>
-            </div>
-            <div className="space-y-2 font-medium text-black text-right">
-              <p>{userData?.organization_id}</p>
-              <p>{userData?.position}</p>
-              <p>{userData?.phone}</p>
-              <p>{userData?.inn || "12345678"}</p>
-            </div>
-          </div>
-        </div>
-      </aside>
-      <aside className="w-full lg:w-[72%]">
-        <Tabs
-          items={profileRightNav}
-          activeKey={activeTab}
-          onChange={(key) => handleMenuClick({ key })}
-          className="profile__tabs-wrapper"
-        />
-
-        <div className="profile__content-card">
-          <Outlet />
-        </div>
-      </aside>
-    </div>
+    <RenderJSX
+      loading={loading}
+      isSnowEnabled={isSnowEnabled}
+      isSettingsOpen={isSettingsOpen}
+      setIsSettingsOpen={setIsSettingsOpen}
+      setIsSnowEnabled={setIsSnowEnabled}
+      isAnimEnabled={isAnimEnabled}
+      setIsAnimEnabled={setIsAnimEnabled}
+      userData={userData}
+      activeTab={activeTab}
+      onMenuClick={handleMenuClick}
+    />
   );
 };
