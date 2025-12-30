d9379aa7a50a145567aa38b13116bb8533f6bf1b
AMIRTJK <jsharipovamir@gmail.com>
2025-12-30 16:24:40 +0500

    fx

--- src/pages/modules/correspondence/CorrespondencePage.tsx
+++ src/pages/modules/correspondence/CorrespondencePage.tsx
@@ -1,13 +1,19 @@
 import { RegistrySidebar } from "@widgets/RegistrySidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export const CorrespondencePage = () => {
+  const { pathname } = useLocation();
+
+  const isCreatePage = pathname.endsWith("/create");
+
+  const heightClass = isCreatePage ? "h-auto" : "h-[800px]";
+
   return (
     <div className="flex w-full gap-6 h-full">
-      <aside className="h-[800px]">
+      <aside className={heightClass}>
         <RegistrySidebar />
       </aside>
-      <main className="w-full h-[800px] rounded-2xl">
+      <main className={`w-full ${heightClass}`}>
         <Outlet />
       </main>
     </div>
