d6c0ef5178901bf680d8f16a0e35fab818cc8dfe
AMIRTJK <jsharipovamir@gmail.com>
2025-12-30 16:11:04 +0500

    fx

--- src/pages/modules/correspondence/CorrespondencePage.tsx
+++ src/pages/modules/correspondence/CorrespondencePage.tsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 
 export const CorrespondencePage = () => {
   return (
-    <div className="flex w-full gap-6 h-full overflow-hidden">
+    <div className="flex w-full gap-6 h-full">
       <aside className="h-[800px]">
         <RegistrySidebar />
       </aside>
-      <main className="w-full h-[800px] rounded-2xl overflow-hidden">
+      <main className="w-full h-[800px] rounded-2xl">
         <Outlet />
       </main>
     </div>
