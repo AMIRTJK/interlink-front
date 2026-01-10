d8c84c1b91b42df4d54cb0b0d0bcd08e81e67e69
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2026-01-11 00:54:12 +0500

    fx

--- src/features/ResolutionOfLetter/ResolutionOfLetter.tsx
+++ src/features/ResolutionOfLetter/ResolutionOfLetter.tsx
@@ -1,10 +1,20 @@
 import { JSX } from "react";
 import { RenderField } from "./lib";
-
+import { Modal } from "antd";
+import './ResolutionOfLetter.css'
 export const ResolutionOfLetter:React.FC<{setIsLetterExecutionVisible: React.Dispatch<React.SetStateAction<boolean>>}> = ({setIsLetterExecutionVisible}):JSX.Element => {
     return (
         <div className="resolution-of-letter__container">
-            <RenderField setIsLetterExecutionVisible={setIsLetterExecutionVisible} resolutionerName="Резолюционер" />
+            <Modal
+            title="Резолюция"
+            open={true}
+            width={1376}
+            onCancel={() => setIsLetterExecutionVisible(false)}
+            footer={null}
+            >
+                <RenderField setIsLetterExecutionVisible={setIsLetterExecutionVisible} resolutionerName="Резолюционер" />
+            </Modal>
+
         </div>
     );
 };
