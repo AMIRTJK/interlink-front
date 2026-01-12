345af87345fb607cc5811053c63f117e8d1284cf
Developer <dev@reaction.local>
2026-01-12 09:43:48 +0500

    ач

--- src/features/ResolutionOfLetter/ResolutionOfLetter.tsx
+++ src/features/ResolutionOfLetter/ResolutionOfLetter.tsx
@@ -1,8 +1,19 @@
 import { JSX } from "react";
 import { RenderField } from "./lib";
 import { Modal } from "antd";
+import { useMutationQuery } from "@shared/lib";
+import { ApiRoutes } from "@shared/api";
 import './ResolutionOfLetter.css'
 export const ResolutionOfLetter:React.FC<{isLetterExecutionVisible: boolean, setIsLetterExecutionVisible: React.Dispatch<React.SetStateAction<boolean>>}> = ({setIsLetterExecutionVisible}):JSX.Element => {
+    const {mutate: chooseResolutionMutate, isPending: chooseResolutionIsPending, isAllowed }=useMutationQuery({
+  url: `ApiRoutes.CREATE_RESOLUTION`,
+  method: "POST",
+  preload: true,
+  preloadConditional: ["correspondence.create"],
+  messages:{
+    invalidate:[ApiRoutes.GET_CORRESPONDENCES]
+  }
+})
     return (
         <div className="resolution-of-letter__container">
             <Modal
@@ -13,7 +24,7 @@ export const ResolutionOfLetter:React.FC<{isLetterExecutionVisible: boolean, set
             onCancel={() => setIsLetterExecutionVisible(false)}
             footer={null}
             >
-                <RenderField resolutionerName="Резолюционер" />
+                <RenderField resolutionerName="Резолюционер" mutate={chooseResolutionMutate} isPending={chooseResolutionIsPending} isAllowed={isAllowed} />
             </Modal>
 
         </div>
