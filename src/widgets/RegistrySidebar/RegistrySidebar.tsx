337cca3f10f08acc7e74d3963ef453fd124c9965
Developer <dev@reaction.local>
2026-01-22 13:35:40 +0500

    fx

--- src/widgets/RegistrySidebar/RegistrySidebar.tsx
+++ src/widgets/RegistrySidebar/RegistrySidebar.tsx
@@ -4,12 +4,10 @@ import { CorrespondenceListSidebar, ModuleSidebar } from "./ui";
 interface IProps {
   isDetailView?: boolean;
 }
-
-// test pull request
 export const RegistrySidebar = ({ isDetailView }: IProps) => {
   return (
     <div className="flex h-full items-stretch">
-      <div className="shrink-0 bg-red-500">
+      <div className="shrink-0">
         <ModuleSidebar isDetailView={isDetailView} />
       </div>
 
