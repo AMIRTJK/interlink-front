1a525b91863bebda05031690000d07f844a4dba9
Developer <dev@reaction.local>
2026-01-30 12:56:13 +0500

    ач

--- src/widgets/DrawerActionsModal/model.tsx
+++ src/widgets/DrawerActionsModal/model.tsx
@@ -1,9 +1,6 @@
 /**
  * Типы данных и списки табов для компонента DrawerActionsModal.
  */
-import { FileTextOutlined, SearchOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
-import { ApiRoutes } from '@shared/api';
-import { SmartDropDown } from '@shared/ui/SmartDropDown';
 import React from 'react';
 
 export interface IActionsModal {
@@ -31,41 +28,3 @@ export const TABS_LIST: { key: TTab; label: string }[] = [
 
 
 // Мок-данные
-export const actionsList: IActionItem[] = [
-   {
-  id: 'blank',
-  label: 'Бланк организации',
-  placeholder: 'Выберите бланк',
-  icon: <FileTextOutlined />,
-  render: () => (
-    <SmartDropDown 
-      url={`${ApiRoutes.GET_FOLDERS}`} 
-      placeholder="Выберите бланк"
-      icon={<FileTextOutlined />}
-      onSelect={(val) => console.log('Выбран ID:', val)}
-      transformResponse={(data: any) => {
-        const list = data?.items || [];
-        return list.map((i: any) => ({ value: i.id, label: i.name }));
-      }}
-    />
-  ),
-},
-    {
-      id: 'attach',
-      label: 'Прикрепить письмо',
-      placeholder: 'Поиск писем для прикрепления',
-      icon: <SearchOutlined />,
-    },
-    {
-      id: 'signer',
-      label: 'Подписывающий',
-      placeholder: 'Выберите подписывающего',
-      icon: <UserOutlined />,
-    },
-    {
-      id: 'approvers',
-      label: 'Согласующие',
-      placeholder: 'Выберите согласующих',
-      icon: <TeamOutlined />,
-    },
-  ];
