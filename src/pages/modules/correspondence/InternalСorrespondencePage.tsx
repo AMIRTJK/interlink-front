4fc26afa39df3f73c45ae5f95f69eeec0d24b6b6
Developer <dev@reaction.local>
2026-01-31 16:47:24 +0500

    fx

--- src/pages/modules/correspondence/InternalСorrespondencePage.tsx
+++ src/pages/modules/correspondence/InternalСorrespondencePage.tsx
@@ -4,6 +4,7 @@ export const InternalСorrespondencePage = () => {
     return <div className="flex gap-3"><h3>Внутренняя корреспонденция</h3>
         <h3>Header Internal-actions</h3>
         {/* Гл компонент */}
+        {/* mode="create" | "show" */}
         <InternalCorrespondece mode="create" />
     </div>;
 };
