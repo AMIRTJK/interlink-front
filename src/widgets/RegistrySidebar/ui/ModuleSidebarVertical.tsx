7fcdcf28dd4e3a8af8fbce0ab3e40dfac2aa8f96
Developer <dev@reaction.local>
2026-02-18 10:37:08 +0500

    fx

--- src/widgets/RegistrySidebar/ui/ModuleSidebarVertical.tsx
+++ src/widgets/RegistrySidebar/ui/ModuleSidebarVertical.tsx
@@ -46,7 +46,7 @@ export const ModuleSidebarVertical: React.FC<IProps> = ({
       trigger={null}
       width="325px"
       collapsedWidth="80px"
-      className={`h-full! border-none! bg-white/50! backdrop-blur-2xl! rounded-3xl! shadow-2xl! shadow-indigo-500/20! ${collapsed ? "p-4!" : "p-6!"} ${
+      className={`min-h-[480px]! border-none! bg-white/50! backdrop-blur-2xl! rounded-3xl! shadow-2xl! shadow-indigo-500/20! ${collapsed ? "p-4!" : "p-6!"} ${
         collapsed ? "w-[80px]! max-w-[80px]!" : "min-w-[300px]! max-w-[330px]!"
       }`}
     >
